a8b03df13e536d8aa96371bc4d1b0ad0412f5a81
Danz-dev <danz124c@gmail.com>
2025-06-08 00:06:24 +0700

    Create mongodb.ts

---
+++ lib/mongodb.ts
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb"
+
+const uri =
+  "mongodb+srv://banjiraki:dE2U3eHdWAvfBjF5@cluster0.rgdn3nr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+let client: MongoClient
+let clientPromise: Promise<MongoClient>
+
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  const globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>
+  }
+
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri)
+    globalWithMongo._mongoClientPromise = client.connect()
+  }
+  clientPromise = globalWithMongo._mongoClientPromise
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri)
+  clientPromise = client.connect()
+}
+
+export default clientPromise
